977f29a2f839d26f9d66ad71acaf8f52900e6f7e
James Santucci <james.santucci@gmail.com>
2016-07-19 17:44:35 -0400

    Refactor examples to run in containers

--- wordcount/wordcount.py
+++ wordcount/wordcount.py
@@ -5,23 +5,36 @@ from luigi.parameter import Parameter
 from luigi.s3 import S3Target, S3Client
 from luigi.contrib.spark import SparkSubmitTask
 
+import ssl
+
+# allow bucket names with dots, see:
+# https://github.com/boto/boto/issues/2836
+
+_old_match_hostname = ssl.match_hostname
+def _new_match_hostname(cert, hostname):
+    if hostname.endswith('.s3.amazonaws.com'):
+        pos = hostname.find('.s3.amazonaws.com')
+        hostname = hostname[:pos].replace('.', '') + hostname[pos:]
+    return _old_match_hostname(cert, hostname)
+
+ssl.match_hostname = _new_match_hostname
+
 VERSION = '0.0.1'
+CLIENT = S3Client(os.getenv('AWS_ACCESS_KEY_ID'),
+                  os.getenv('AWS_SECRET_ACCESS_KEY'))
 
 class UploadFile(luigi.Task):
 
     version = Parameter(default=VERSION)
     input_file = Parameter(default='frankenstein.txt')
 
-    client = S3Client(os.getenv('AWS_ACCESS_KEY_ID'),
-                      os.getenv('AWS_SECRET_ACCESS_KEY'))
-
     def run(self):
-        self.client.put(self.input_file,
-                        's3://test.objects/' + self.input_file)
+        CLIENT.put(self.input_file,
+                   's3://test.objects/' + self.input_file)
         self.complete()
 
     def complete(self):
-        if self.client.exists('s3://test.objects/' + self.input_file):
+        if CLIENT.exists('s3://test.objects/' + self.input_file):
             return True
         return False
 
@@ -52,6 +65,23 @@ class CountWords(SparkSubmitTask):
 
     def app_options(self):
         return [self.input().path, self.output().path]
+
+class RunAll(luigi.Task):
+
+    version = Parameter(default=VERSION)
+    input_file = Parameter(default='frankenstein.txt')
+
+    def requires(self):
+        return CountWords(version=self.version, input_file=self.input_file)
+
+    def output(self):
+        return S3Target('s3://test.objects/wc_' + self.input_file)
+
+    def run(self):
+        CLIENT.put('wc_' + self.input_file,
+                   's3://test.objects/wc_' + self.input_file)
+        os.remove('wc_' + self.input_file)
+
 
 if __name__ == '__main__':
     luigi.run()
